2880c3d9676142a8399402ba9a6b8696127c44b6
Seiya Umemoto <50127194+Seiya-Umemoto@users.noreply.github.com>
2021-02-15 16:33:18 +0900

    Update namingfiles.py
    changed index 0 to -1

--- images/namingfiles.py
+++ images/namingfiles.py
@@ -4,7 +4,7 @@ import re
 
 files = glob("/home/seiya/yoloboard/cus_data/test_images/*.jpg")
 
-files = sorted(files, key=lambda file: int(re.findall("[0-9]+", file)[0]))
+files = sorted(files, key=lambda file: int(re.findall("[0-9]+", file)[-1]))
 
 with open('/home/seiya/yoloboard/cus_data/cus_testing.txt', 'w') as f:
     for file in files:
